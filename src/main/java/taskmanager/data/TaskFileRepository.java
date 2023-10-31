38e85c8391d6c5e48a311839fb53efb7a8fe9368
AErdenberger <erdenberger.alexander@gmail.com>
2023-10-31 14:45:10 -0700

    serialize and deserialize helpers made

--- src/main/java/taskmanager/data/TaskFileRepository.java
+++ src/main/java/taskmanager/data/TaskFileRepository.java
@@ -1,5 +1,6 @@
 package taskmanager.data;
 
+import taskmanager.models.Status;
 import taskmanager.models.Task;
 
 import java.util.List;
@@ -48,4 +49,44 @@ public class TaskFileRepository implements TaskRepository{
     private String clean(String value){
         return value.replace(DELIMETER, DELIMETER_REPLACEMENT);
     }
+
+    //deserialize data
+    private Task lineToTask(String line){
+        String[] fields = line.split(DELIMETER);
+        if(fields.length != 6){
+            return null;
+        }
+
+        Task task = new Task(
+                Integer.parseInt(fields[0]),
+                restore(fields[1]),
+                restore(fields[2]),
+                restore(fields[3]),
+                restore(fields[4]),
+                Status.valueOf(fields[5])
+        );
+
+        return task;
+    }
+
+    //serializing tasks
+    private String taskToLine(Task task){
+        //add a comma after each field
+        StringBuilder buffer = new StringBuilder(100);
+        buffer.append(task.getId()).append(DELIMETER);
+
+        /*
+        this is the line that would look at each date which is a string
+        if there are ',' we would replace that with @@@ here
+         */
+        buffer.append(clean(task.getCreatedOn())).append(DELIMETER);
+        buffer.append(clean(task.getTitle())).append(DELIMETER);
+        buffer.append(clean(task.getDescription())).append(DELIMETER);
+        buffer.append(clean(task.getDoDate())).append(DELIMETER);
+        buffer.append(task.getStatus());
+
+        return buffer.toString();
+    }
+
+
 }
