4d59fbeabc3937e12de58a331fd9159d9d29fdae
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-01 16:05:25 -0700

    tests written

--- src/test/java/taskmanager/domain/TaskServiceTest.java
+++ src/test/java/taskmanager/domain/TaskServiceTest.java
@@ -1,7 +1,43 @@
 package taskmanager.domain;
 
+import org.junit.jupiter.api.Test;
+import taskmanager.data.TaskRepository;
+import taskmanager.data.TaskRepositoryDouble;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskServiceTest {
+
+    TaskRepository repository = new TaskRepositoryDouble();
+    TaskService service = new TaskService(repository);
+
+    //CREATE
+    //Happy Path
+    @Test
+    public void shouldCreateTask(){
+
+    }
+
+    //READ
+    //Happy Path
+    @Test
+    public void shouldFindAll(){
+
+    }
+
+    @Test
+    public void shouldFindExistingID(){
+
+    }
+
+    //Sad Path
+    @Test
+    public void shouldNotFindNonExistingID(){
+
+    }
+
+    //UPDATE
+
+    //DELETE
 
 }
