1460e96ee9e04e38e61e96bc0cf0470763ce6931
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-01 15:49:59 -0700

    TaskService methods created, TaskServiceTest created

--- src/main/java/taskmanager/domain/TaskService.java
+++ src/main/java/taskmanager/domain/TaskService.java
@@ -1,4 +1,25 @@
 package taskmanager.domain;
 
+import taskmanager.data.DataAccessException;
+import taskmanager.data.TaskRepository;
+import taskmanager.models.Task;
+
+import java.util.List;
+
 public class TaskService {
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository){
+        this.repository = repository;
+    }
+
+    //read
+    public List<Task> findAll() throws DataAccessException {
+        return repository.findAll();
+    }
+
+    public Task findByID(int taskID) throws DataAccessException {
+        return repository.findByID(taskID);
+    }
 }
---
+++ src/test/java/taskmanager/domain/TaskServiceTest.java
@@ -0,0 +1,7 @@
+package taskmanager.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+}
