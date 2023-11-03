e7dc62ecafc7dd7639320d10ae0231c3a0af5288
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-03 15:51:15 -0700

    App functioning properly

--- src/main/java/taskmanager/App.java
+++ src/main/java/taskmanager/App.java
@@ -2,17 +2,19 @@ package taskmanager;
 
 import taskmanager.data.DataAccessException;
 import taskmanager.data.TaskFileRepository;
+import taskmanager.domain.TaskService;
 import taskmanager.models.Task;
+import taskmanager.ui.Controller;
+import taskmanager.ui.View;
 
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws DataAccessException {
-        TaskFileRepository repository = new TaskFileRepository("C:\\Users\\erden\\IdeaProjects\\TaskManager\\data\\tasks.csv");
-        List<Task> tasks = repository.findAll();
-
-        for (Task task : tasks){
-            System.out.println(task);
-        }
+    public static void main(String[] args) {
+        TaskFileRepository repository = new TaskFileRepository("./data/tasks.csv");
+        TaskService service = new TaskService(repository);
+        View view = new View();
+        Controller controller = new Controller(view, service);
+        controller.run();
     }
 }
