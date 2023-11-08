6b3f2a212504af0af79977a89e34f3bfbd5fad5a
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-08 13:48:05 -0800

    refactored for SpringXML

--- src/main/java/taskmanager/App.java
+++ src/main/java/taskmanager/App.java
@@ -1,5 +1,8 @@
 package taskmanager;
 
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import taskmanager.data.DataAccessException;
 import taskmanager.data.TaskFileRepository;
 import taskmanager.domain.TaskService;
@@ -11,10 +14,21 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        TaskFileRepository repository = new TaskFileRepository("./data/tasks.csv");
-        TaskService service = new TaskService(repository);
-        View view = new View();
-        Controller controller = new Controller(view, service);
+//        TaskFileRepository repository = new TaskFileRepository("./data/tasks.csv");
+//        TaskService service = new TaskService(repository);
+//        View view = new View();
+//        Controller controller = new Controller(view, service);
+//        controller.run();
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("dependency-config.xml");
+        Controller controller = container.getBean(Controller.class);
         controller.run();
+    }
+
+    //XML DEPENDENCY INJECTION
+
+    //MANUAL DEPENDENCY INJECTION
+    private static void configureManuallyAndRun(){
+
     }
 }
